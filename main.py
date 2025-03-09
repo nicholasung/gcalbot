00ef27da8fc17750996897dbf204d1d613c56c6b
nicholasung <nicholas.ung0@gmail.com>
2025-03-08 16:11:03 -0800

    i can manipulate time!

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv
 # import io
 # import discord
 import os
-from datetime import datetime, timezone
-from dateutil.relativedelta import relativedelta
+from datetime import datetime, timezone, date, time, timedelta
 
 # Load environment variables from .env file
 load_dotenv()
@@ -23,10 +22,11 @@ credentials = Credentials.from_service_account_file(credentials_path)
 
 #get calendar
 gc = GoogleCalendar(credentials=credentials)
-calendar = gc.get_calendar(cal_id)
+gc.default_calendar=cal_id
+calendar = gc #.get_calendar(cal_id)
 
 # Fetch and print events from the specified calendar
-events = list(gc.get_events(calendar_id=cal_id))
+events = list(gc.get_events())
 print(f"Found {len(events)} events in calendar {cal_id}.")
 
 def datetimeToStr(datetime):
@@ -34,4 +34,19 @@ def datetimeToStr(datetime):
 
 for event in events:
     print(event.summary + " " + datetimeToStr(event.start))
+
+year = 2025
+month = 3
+day = 9
+hour = 13
+min = 25
+hour_duration = 2.5
+
+start = datetime(year, month, day, hour, min)
+end = start + timedelta(hours=hour_duration)
+event = Event('test', start, end)
+
+# print(event.summary + " " + datetimeToStr(event.start) + " end at " + datetimeToStr(event.end))
+response = gc.add_event(event)
+print(response)
 
