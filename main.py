33154b0708f8dd089fa8937fd5ba74b5c42fd546
nicholasung <nicholas.ung0@gmail.com>
2025-03-08 17:17:05 -0800

    can read events from discord

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from gcsa.event import Event
 from google.oauth2.service_account import Credentials
 from gcsa.google_calendar import GoogleCalendar
 from dotenv import load_dotenv
-# from discord.ext import commands
-# from discord import app_commands
-# import io
-# import discord
+from discord.ext import commands
+from discord import app_commands
+import io
+import discord
 import os
 from datetime import datetime, timezone, date, time, timedelta
 
@@ -16,45 +16,58 @@ cal_id = os.getenv('CAL_ID')
 print(token)
 print(cal_id)
 
-#authenticate
+# authenticate
 credentials_path = ".credentials/credentials.json"
 credentials = Credentials.from_service_account_file(credentials_path)
 
-#get calendar
+# get calendar
 gc = GoogleCalendar(credentials=credentials)
 gc.default_calendar=cal_id
-calendar = gc #.get_calendar(cal_id)
+calendar = gc
+
+# Discord bot init 
+intents = discord.Intents.default()
+intents.guild_messages = True
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
 
 # Fetch and print events from the specified calendar
 events = list(gc.get_events())
 print(f"Found {len(events)} events in calendar {cal_id}.")
 
+
 def datetimeToStr(datetime):
     return datetime.strftime('%Y-%m-%d %H:%M:%S')
 
-for event in events:
-    print(event.summary + " " + datetimeToStr(event.start))
-    if event.summary == "test":
-        update = event
+def eventFromName(name):
+    for event in list(gc.get_events()):
+            if event.summary == name:
+                result = event
+    return result
 
-# year = 2025
-# month = 3
-# day = 9
-# hour = 13
-# min = 25
-# hour_duration = 2.5
+@tree.command(name="status", description="Check the bot's status")
+async def status(interaction: discord.Interaction):
+     await interaction.response.send_message('master manipulator is up', ephemeral=True)
+     print(interaction.user.name + " requested status")
 
-# start = datetime(year, month, day, hour, min)
-# end = start + timedelta(hours=hour_duration)
-# event = Event('test', start, end)
+@tree.command(name="events", description="list all the current events in the server")
+async def events(interaction:discord.Interaction):
+    reply = ""
+    await interaction.response.defer()
+    list = await interaction.guild.fetch_scheduled_events()
+    print(interaction.user.name + " requested events")
+    for thing in list:
+        print(thing.name)
+        reply = reply + thing.name + " | "
+    await interaction.followup.send(reply, ephemeral = False)
 
-# print(event.summary + " " + datetimeToStr(event.start) + " end at " + datetimeToStr(event.end))
-# response = gc.add_event(event)
-# print(response)
+@bot.event
+async def on_ready():
+    await tree.sync()
+    print('We have logged in and synced')
+    print('Synced commands:')
+    for command in tree.walk_commands():
+        print(f"- {command.name}: {command.description}")
 
-
-update.location = "OVER HERE"
-
-response = gc.update_event(update)
-print(response)
+bot.run(token)
 
